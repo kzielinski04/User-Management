ad2694a78158ca3ac12ec8b35f8815d87f1a0c1e
Kacper Zieliński <zielinski_kacper@outlook.com>
2024-12-07 01:26:44 +0100

    feat: add load_users function

--- user_management.py
+++ user_management.py
@@ -1,1 +1,11 @@
 import json, os, random, re
+
+USERS_PATH = "data/users.json"
+
+def load_users() -> list:
+    """Load users from users.json"""
+    if not os.path.exists(USERS_PATH):
+        return []
+    with open(USERS_PATH, 'r') as file:
+        users = json.load(USERS_PATH)
+        return users
