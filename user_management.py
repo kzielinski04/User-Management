3bb09a59d276df3d28e6eb86bbb151616dd86842
Kacper Zieliński <zielinski_kacper@outlook.com>
2024-12-07 03:01:44 +0100

    feat: declare validation functions

--- user_management.py
+++ user_management.py
@@ -2,6 +2,15 @@ import json, os, random, re
 
 USERS_PATH = "data/users.json"
 
+def validate_pesel(user_pesel:str) -> bool:
+    pass
+
+def validate_nip(user_nip:str) -> bool:
+    pass
+
+def validate_regon(user_regon:str) -> bool:
+    pass
+
 def load_users() -> list:
     """Load users from users.json"""
     if not os.path.exists(USERS_PATH):
@@ -12,6 +21,8 @@ def load_users() -> list:
     
 def add_user(user_data:dict):
     """Add new user to users.json"""
+    if validate_pesel(user_data["user_pesel"]) == False or validate_nip(user_data["user_nip"]) == False or validate_regon(user_data["user_regon"]) == False:
+        return
     users = load_users()
     users.append(user_data)
     with open(USERS_PATH, 'w') as file:
@@ -27,6 +38,8 @@ def remove_user(user_id:int):
 
 def edit_user(user_id:int, updated_data:dict):
     """Edit user from user.json"""
+    if validate_pesel(updated_data["user_pesel"]) == False or validate_nip(updated_data["user_nip"]) == False or validate_regon(updated_data["user_regon"]) == False:
+        return
     users = load_users()
     for user in users:
         if user["user_id"] == user_id:
