521aebc0fdf217cc17576b0df3b2f281784a7901
Kacper Zieliński <zielinski_kacper@outlook.com>
2024-12-07 01:43:34 +0100

    feat: add add_user function

--- user_management.py
+++ user_management.py
@@ -7,5 +7,12 @@ def load_users() -> list:
     if not os.path.exists(USERS_PATH):
         return []
     with open(USERS_PATH, 'r') as file:
-        users = json.load(USERS_PATH)
+        users = json.load(file)
         return users
+    
+def add_user(user_data:dict):
+    """Add new user to users.json"""
+    users = load_users()
+    users.append(user_data)
+    with open(USERS_PATH, 'w') as file:
+        json.dump(users, file)
